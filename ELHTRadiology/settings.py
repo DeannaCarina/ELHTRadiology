0aaad75ae230861733a148999ac82c24c6d7c717
DeannaCarina <deannacarina@hotmail.com>
2021-11-21 20:35:49 +0000

    update: Database settings in settings.py

--- ELHTRadiology/settings.py
+++ ELHTRadiology/settings.py
@@ -68,11 +68,15 @@ WSGI_APPLICATION = 'ELHTRadiology.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
