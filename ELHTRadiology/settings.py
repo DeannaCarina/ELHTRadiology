6f6a671470271483b47613721a48170de066f2f0
DeannaCarina <deannacarina@hotmail.com>
2021-11-30 16:20:17 +0000

    2nd attempt fix for first deployment static file fix

--- ELHTRadiology/settings.py
+++ ELHTRadiology/settings.py
@@ -115,10 +115,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+
+
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+
+
+
 
 
 
