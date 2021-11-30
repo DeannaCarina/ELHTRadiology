f50210d064b8547e42385b740661f4ca1ae2a391
DeannaCarina <deannacarina@hotmail.com>
2021-11-30 16:38:32 +0000

    add: BASE_DIR var to settings.py

--- ELHTRadiology/settings.py
+++ ELHTRadiology/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
