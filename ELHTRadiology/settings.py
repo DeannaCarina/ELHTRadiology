81fec03a5e9188a050ced0398e4de9be447cb41d
DeannaCarina <deannacarina@hotmail.com>
2021-12-02 11:56:34 +0000

    Revert BASE_DIR var in settings.py

--- ELHTRadiology/settings.py
+++ ELHTRadiology/settings.py
@@ -6,9 +6,7 @@ if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
