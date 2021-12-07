749ac0a57527095ec77b66a0e85a8724e6ed214a
DeannaCarina <deannacarina@hotmail.com>
2021-12-07 22:08:23 +0000

    Add all pages (expect booking forms) to website and link up

--- booking/urls.py
+++ booking/urls.py
@@ -1,7 +1,30 @@
 
 from django.urls import path
-from .views import HomeTemplateView
+from .views import HomeTemplateView, PrivacyTemplateView, ContactTemplateView, AdminTemplateView, InformationTemplateView, RequestsTemplateView, LocationsTemplateView, RequestsTemplateView, RadiationTemplateView, ResultsTemplateView, DepartmentTemplateView, ExamboardTemplateView, XrayTemplateView, CtTemplateView, MriTemplateView, UsTemplateView, NmTemplateView, MammoTemplateView, FluoroTemplateView, AngioTemplateView
 
 urlpatterns = [
     path('', HomeTemplateView.as_view(), name='home'),
+    path('privacy', PrivacyTemplateView.as_view(), name='privacy'),
+    path('contact', ContactTemplateView.as_view(), name='contact'),
+    path('admin/login/?next=/admin/', AdminTemplateView.as_view(), name='admin'),
+
+
+    
+
+    path('information', InformationTemplateView.as_view(), name='information'),
+    path('information/requests', RequestsTemplateView.as_view(), name='requests'),
+    path('information/locations', LocationsTemplateView.as_view(), name='locations'),
+    path('information/radiation', RadiationTemplateView.as_view(), name='radiation'),
+    path('information/results', ResultsTemplateView.as_view(), name='results'),
+    path('information/department', DepartmentTemplateView.as_view(), name='department'),
+
+    path('examinations', ExamboardTemplateView.as_view(), name='examinations'),
+    path('examinations/xray', XrayTemplateView.as_view(), name='xray'),
+    path('examinations/computed_tomography', CtTemplateView.as_view(), name='computed_tomography'),
+    path('examinations/magnetic_resonance', MriTemplateView.as_view(), name='magnetic_resonance'),
+    path('examinations/ultrasound', UsTemplateView.as_view(), name='ultrasound'),
+    path('examinations/mammography', MammoTemplateView.as_view(), name='mammography'),
+    path('examinations/nuclear_medicine', NmTemplateView.as_view(), name='nuclear_medicine'),
+    path('examinations/fluoroscopy', FluoroTemplateView.as_view(), name='fluoroscopy'),
+    path('examinations/angiography', AngioTemplateView.as_view(), name='angiography'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -4,3 +4,65 @@ from django.views.generic.base import TemplateView
 
 class HomeTemplateView(TemplateView):
     template_name = 'index.html'
+
+class PrivacyTemplateView(TemplateView):
+    template_name = 'privacy.html'
+
+class ContactTemplateView(TemplateView):
+    template_name = 'contact.html'
+
+
+class AdminTemplateView(TemplateView):
+    template_name = 'admin.html'
+
+
+
+
+# Classes for information pages
+
+class InformationTemplateView(TemplateView):
+    template_name = 'information/information.html'
+
+class RequestsTemplateView(TemplateView):
+    template_name = 'information/requests.html'
+
+class LocationsTemplateView(TemplateView):
+    template_name = 'information/locations.html'
+
+class RadiationTemplateView(TemplateView):
+    template_name = 'information/radiation.html'
+
+class ResultsTemplateView(TemplateView):
+    template_name = 'information/results.html'
+
+class DepartmentTemplateView(TemplateView):
+    template_name = 'information/department.html'
+
+# Classes for examination pages
+
+class ExamboardTemplateView(TemplateView):
+    template_name = 'examinations/examinations.html'
+
+class XrayTemplateView(TemplateView):
+    template_name = 'examinations/xray.html'
+
+class CtTemplateView(TemplateView):
+    template_name = 'examinations/computed_tomography.html'
+
+class MriTemplateView(TemplateView):
+    template_name = 'examinations/magnetic_resonance.html'
+
+class NmTemplateView(TemplateView):
+    template_name = 'examinations/nuclear_medicine.html'
+
+class MammoTemplateView(TemplateView):
+    template_name = 'examinations/mammography.html'
+
+class AngioTemplateView(TemplateView):
+    template_name = 'examinations/angiography.html'
+
+class UsTemplateView(TemplateView):
+    template_name = 'examinations/ultrasound.html'
+
+class FluoroTemplateView(TemplateView):
+    template_name = 'examinations/fluoroscopy.html'
