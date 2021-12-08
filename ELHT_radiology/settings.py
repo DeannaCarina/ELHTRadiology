7765ea640277a3b8c2666904647ad8ec5596a263
DeannaCarina <deannacarina@hotmail.com>
2021-12-08 10:21:43 +0000

    Install whitenoise to serve static files

--- ELHT_radiology/settings.py
+++ ELHT_radiology/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ELHT_radiology.urls'
