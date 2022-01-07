636f275f2e240c7622ea79d76ec3394217acec4d
DeannaCarina <deannacarina@hotmail.com>
2022-01-07 10:19:40 +0000

    remove: Dev databases from settings

--- ELHT_radiology/settings.py
+++ ELHT_radiology/settings.py
@@ -103,17 +103,17 @@ WSGI_APPLICATION = 'ELHT_radiology.wsgi.application'
 #     }
 # }
 
-if development:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+# if development:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 
 # Password validation
