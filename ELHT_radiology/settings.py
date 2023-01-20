d7e71d2301f11c1c2f5cdad24cec56b2628c5abc
DeannaCarina <deannacarina@hotmail.com>
2023-01-20 20:19:01 +0000

    Set up for migration to Render

--- ELHT_radiology/settings.py
+++ ELHT_radiology/settings.py
@@ -38,6 +38,12 @@ if development:
 else:
     ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
 # Application definition
 
 INSTALLED_APPS = [
--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn ELHT_radiology.wsgi
